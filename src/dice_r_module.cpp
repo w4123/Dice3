1d113bca0e308ca50ad5c257c186af2e9aa4ca57
w4123 <1840686745@qq.com>
2019-09-10 18:23:19 +0800

    Basic Event Structure

---
+++ src/dice_r_module.cpp
@@ -0,0 +1,23 @@
+#include "dice_r_module.h"
+#include "cqsdk/cqsdk.h"
+#include <regex>
+#include <string>
+
+namespace cq::event {
+    struct MessageEvent;
+}
+
+namespace dice {
+    bool r_module::match(const cq::event::MessageEvent& e, const std::wstring& ws) {
+        std::wregex re(L"[\\.。]r.*", std::regex_constants::ECMAScript | std::regex_constants::icase);
+        return std::regex_match(ws, re);
+    }
+    void r_module::process(const cq::event::MessageEvent& e, const std::wstring& ws) {  
+        std::wregex re(L"[\\.。]r([0-9d+\\-*x/×÷\\(\\)]*)(.*)",
+                      std::regex_constants::ECMAScript | std::regex_constants::icase);
+        std::wsmatch m;
+        auto ret = std::regex_match(ws, m, re);
+        if (m[1].first != m[1].second)
+            cq::api::send_msg(e.target, cq::utils::ws2s(std::wstring(m[1].first, m[1].second)));
+    }
+} // namespace dice
